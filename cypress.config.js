ed7b0de0e9f8a94bccd79be61513d82cf34c46fc
xywang68 <xywang68@gmail.com>
2023-01-29 17:27:47 -0800

    reorg folders

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    specPattern: 'src/ui-test/',
     // uncomment below will print the cypress config
     // setupNodeEvents(on, config) {
     //   console.log(config)
